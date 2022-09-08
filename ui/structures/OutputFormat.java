95355024b5e6b855ca187c4c4a1946f252faf1f3
etruyj <etruyj@gmail.com>
2022-09-08 14:55:18 -0400

    added display structure code

---
+++ ui/structures/OutputFormat.java
@@ -0,0 +1,16 @@
+//===================================================================
+// OutputFormat.java
+// 	Description:
+// 		This is the variable input that is required for all
+// 		ui.display functions. Any output should be converted
+// 		from the input variable to this key value pair set-up
+//===================================================================
+
+package com.socialvagrancy.utils.ui.structures;
+
+public class OutputFormat
+{
+	public String key;
+	public String value;
+	public int indents;
+}
