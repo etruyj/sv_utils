88b55ae3b1311f95388af7c905b1767bf8493cca
etruyj <etruyj@gmail.com>
2024-03-08 11:19:00 -0500

    Shifted all request functions to a central request() class to allow centralized error handling.

--- src/main/java/com/socialvagrancy/http/RestApi.java
+++ src/main/java/com/socialvagrancy/http/RestApi.java
@@ -16,6 +16,8 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.security.cert.X509Certificate;
 
+import org.apache.hc.client5.http.HttpResponseException;
+
 public class RestApi
 {
 	public RestApi(boolean ignore_ssl)
@@ -58,439 +60,42 @@ public class RestApi
 
 	public String authenticate(String httpRequest, String body) throws Exception
 	{
-
-		// Query library without a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-			
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("POST");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-	
-			OutputStream output = cxn.getOutputStream();
-			byte[] input = body.getBytes("utf-8");
-			output.write(input, 0, input.length);
-			
-			
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{	
-			throw new Exception(e.getMessage());
-		}
-
-		return response.toString();
+	    return request("POST", httpRequest, "NO_AUTH", null, body);
 	}
 	
-/*	public String basicAuth(String httpRequest, String username, char[] password) throws Exception
-	{
-
-		// Query library without a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		
-		// Open connection		
-		try
-		{
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("POST");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json; utf-8");
-			cxn.setRequestProperty("Accept", "application/json");
-	
-			OutputStream output = cxn.getOutputStream();
-			byte[] input = body.getBytes("utf-8");
-			output.write(input, 0, input.length);
-			
-			
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{	
-			throw new Exception(e.getMessage());
-		}
-
-		return response.toString();
-	}
-*/	
 	public String delete(String httpRequest, String token) throws Exception
 	{
-		// Send DELETE command to the server.
-
-		StringBuilder response = new StringBuilder();
-		int response_code = 0;
-
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-			
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("DELETE");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			cxn.setRequestProperty("Authorization", token);
-
-			// GET response code.
-			response_code = cxn.getResponseCode();
-
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		// Check to see if there is a response.
-		// If not, return the response code instead.
-		if(response.length()==0)
-		{
-			throw new Exception("[" + response_code + "] Failed to delete.");
-		}
-		else
-		{
-			return response.toString();
-		}
+	    return request("DELETE", httpRequest, "Authorization", token, null);
 	}
 
 	public String get(String httpRequest) throws Exception
 	{
-		// Query library with a header (authorization)
-		// mostly just for logging in.
-		StringBuilder response = new StringBuilder();
-		int response_code = 0;
-
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-			
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("GET");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			
-			// GET response code.
-			response_code = cxn.getResponseCode();
-			
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		return response.toString();
-	}
+	    return request("GET", httpRequest, "NO_AUTH", null, null);
+    }
 	
 	public String get(String httpRequest, String token) throws Exception
 	{
-		// Query library with a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		int response_code = 0;
-
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("GET");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			cxn.setRequestProperty("Authorization", token);
-			
-			// GET response code.
-			response_code = cxn.getResponseCode();
-
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		// Check to see if there is a response.
-		// If not, return the response code instead.
-		if(token.equals("code"))
-		{
-			throw new Exception("[" + response_code + "] Unable to complete GET request.");
-		}
-		else
-		{
-			return response.toString();
-		}
-	}
+	    return request("GET", httpRequest, "Authorization", token, null);
+    }
 	
 	public String get(String httpRequest, String token, String auth_header) throws Exception
 	{
-		// Query library with a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		int response_code = 0;
-
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("GET");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			cxn.setRequestProperty(auth_header, token);
-			
-			// GET response code.
-			response_code = cxn.getResponseCode();
-
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		// Check to see if there is a response.
-		// If not, return the response code instead.
-		if(token.equals("code"))
-		{
-			throw new Exception("[" + response_code + "] Unable to complete GET request.");
-		}
-		else
-		{
-			return response.toString();
-		}
+        return request("GET", httpRequest, auth_header, token, null);
 	}
 	
 	public String post(String httpRequest, String token, String auth_header, String body) throws Exception
 	{
-		// Query library without a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-			
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("POST");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			cxn.setRequestProperty(auth_header, token);
-
-			if(body.length() > 0)
-			{
-				OutputStream output = cxn.getOutputStream();
-				byte[] input = body.getBytes("utf-8");
-				output.write(input, 0, input.length);
-			}
-			
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		return response.toString();
-	}
+	    return request("POST", httpRequest, auth_header, token, body);
+    }
 	
 	public String post(String httpRequest, String token, String body) throws Exception
 	{
-		// Query library without a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-			
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("POST");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			cxn.setRequestProperty("Authorization", token);
-
-			if(body.length() > 0)
-			{
-				OutputStream output = cxn.getOutputStream();
-				byte[] input = body.getBytes("utf-8");
-				output.write(input, 0, input.length);
-			}
-			
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		return response.toString();
-	}
+	    return request("POST", httpRequest, "Authorization", token, body);
+    }
 	
 	public String put(String httpRequest, String token, String body) throws Exception
 	{
-		// Query library without a header (authorization)
-		// mostly just for logging in.
-
-		StringBuilder response = new StringBuilder();
-		
-		// Open connection		
-		try
-		{
-			// Remove spaces from httpRequest
-			httpRequest = parseUrl(httpRequest);
-			
-			URL url = new URL(httpRequest);
-			HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		
-			// Configuration the connection
-			cxn.setRequestMethod("PUT");
-			cxn.setDoOutput(true);
-			cxn.setRequestProperty("Content-Type", "application/json");
-			cxn.setRequestProperty("Accept", "application/json");
-			cxn.setRequestProperty("Authorization", token);
-
-			if(body.length() > 0)
-			{
-				OutputStream output = cxn.getOutputStream();
-				byte[] input = body.getBytes("utf-8");
-				output.write(input, 0, input.length);
-			}
-			
-			// Read response	
-			BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
-
-			String responseLine = null;
-
-			while((responseLine = br.readLine()) != null)
-			{
-				response.append(responseLine);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new Exception(e.getMessage());
-		}
-
-		return response.toString();
+	    return request("PUT", httpRequest, "Authorization", token, body);
 	}
 
 	//=======================================
@@ -500,5 +105,52 @@ public class RestApi
 	private String parseUrl(String url)
 	{
 		return url.replace(" ", "%20");
+	}
+	
+    private String request(String method, String httpRequest, String auth_type, String token, String body)
+	{
+		StringBuilder response = new StringBuilder();
+		
+		// Open connection		
+		// Remove spaces from httpRequest
+		httpRequest = parseUrl(httpRequest);
+		
+        try {    
+		    URL url = new URL(httpRequest);
+		    HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
+		
+		    // Configuration the connection
+		    cxn.setRequestMethod(method);
+		    cxn.setDoOutput(true);
+		    cxn.setRequestProperty("Content-Type", "application/json");
+		    cxn.setRequestProperty("Accept", "application/json");
+		    
+            if(!auth_type.equals("NO_AUTH")) {
+                cxn.setRequestProperty(auth_type, token);
+            }
+
+		    if(body != null && body.length() > 0)
+		    {
+		    	OutputStream output = cxn.getOutputStream();
+			    byte[] input = body.getBytes("utf-8");
+			    output.write(input, 0, input.length);
+		    }
+			
+		    // Read response	
+		    BufferedReader br = new BufferedReader(new InputStreamReader(cxn.getInputStream(), "utf-8"));
+
+		    String responseLine = null;
+
+		    while((responseLine = br.readLine()) != null)
+		    {
+			    response.append(responseLine);
+		    }
+        } catch(HttpResponseException e) {
+            System.err.println("[" + e.getStatusCode() + "] " + e.getMessage());
+        } catch(IOException e) {
+            System.err.println("[IOException] " + e.getMessage());
+        }
+
+		return response.toString();
 	}
 }
